cd377edc526ac22342e995815e037241e7e830ec
ITSK1MLOT <itskimlot@gmail.com>
2023-12-31 21:14:50 +0900

    update: display version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     testImplementation(kotlin("test"))
 
     // MINECRAFT DEPENDENCIES
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT")
     // JAVA DEPENDENCIES
     implementation(kotlin("stdlib-jdk8"))
 }
